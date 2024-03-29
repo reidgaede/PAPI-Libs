090cab9d112d20f856c48fae80b3db37d7eca5cb
Reid Gaede <126724982+reidgaede@users.noreply.github.com>
2024-03-28 22:41:38 -0400

    Updated documentation in "CorporateBuzzwords.cs".

--- Models/CorporateBuzzwords.cs
+++ Models/CorporateBuzzwords.cs
@@ -1,5 +1,5 @@
-/* (3/17/24, 16) Again, used https://wtools.io/convert-json-to-csharp-class to "reverse-engineer" the JSON returned by 
-one of the publicly-accessible APIs I found into a C# class. */
+/* Used https://wtools.io/convert-json-to-csharp-class to reverse-engineer the JSON returned by
+this publicly-accessible API into a C# class. */
 using System.Text.Json.Serialization;
 
 namespace PAPI_Libs.Models;
